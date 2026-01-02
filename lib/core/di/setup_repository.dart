5b1e69c1c9d957169be1f0937c5fa255942c2ecf
Soykot Rahman Rodro <soykotrahman1212@gmail.com>
2026-01-02 10:29:52 +0600

    add new files

--- lib/core/di/setup_repository.dart
+++ lib/core/di/setup_repository.dart
@@ -1,11 +1,25 @@
 import 'package:get/get.dart';
 import '../../features/auth/data/repo/auth_repo_impl.dart';
 import '../../features/auth/domain/repo/auth_repo.dart';
+import '../../features/todos/data/repositories/todo_category_repository_impl.dart';
+import '../../features/todos/domain/repositories/todo_category_repository.dart';
+import '../../features/todos/data/repositories/todo_item_repository_impl.dart';
+import '../../features/todos/domain/repositories/todo_item_repository.dart';
 
 
 void setupRepository() {
   Get.lazyPut<AuthRepository>(
     fenix: true,
     () => AuthRepositoryImpl(apiClient: Get.find()),
+  );
+
+  Get.lazyPut<TodoCategoryRepository>(
+    fenix: true,
+    () => TodoCategoryRepositoryImpl(apiClient: Get.find()),
+  );
+
+  Get.lazyPut<TodoItemRepository>(
+    fenix: true,
+    () => TodoItemRepositoryImpl(apiClient: Get.find()),
   );
 }
