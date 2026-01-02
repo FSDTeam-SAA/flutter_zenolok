36ac336d36fae33dab1b51188150cdfe04373ffd
Soykot Rahman Rodro <soykotrahman1212@gmail.com>
2026-01-02 10:31:28 +0600

    add new files and designs

---
+++ lib/features/todos/domain/repositories/todo_category_repository.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_zenolok/core/network/models/network_failure.dart';
+import 'package:flutter_zenolok/core/network/models/network_success.dart';
+import '../../data/models/category_model.dart';
+
+abstract class TodoCategoryRepository {
+  /// Fetches all todo categories
+  /// Returns either a [NetworkFailure] or a list of [CategoryModel]
+  Future<Either<NetworkFailure, NetworkSuccess<List<CategoryModel>>>> getAllCategories();
+
+  /// Creates a new todo category
+  /// Returns either a [NetworkFailure] or the created [CategoryModel]
+  Future<Either<NetworkFailure, NetworkSuccess<CategoryModel>>> createCategory({
+    required String name,
+    required String color,
+  });
+}
