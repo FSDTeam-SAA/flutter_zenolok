35a100e62f12b1dd7a843c13b2bd83d87b382d73
Soykot Rahman Rodro <soykotrahman1212@gmail.com>
2026-01-03 10:06:33 +0600

    just delete comment part from the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,49 +23,5 @@ class MyApp extends StatelessWidget {
       initialBinding: InitialBinding(),
       home: const SplashScreen(),
     );
-
-
   }
-
 }
-
-
-// // //------------------------------------------
-
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_zenolok/features/auth/presentation/screens/login_screen.dart';
-
-// void main() => runApp(const CalendarApp());
-
-// class CalendarApp extends StatelessWidget {
-//   const CalendarApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final scheme = ColorScheme.fromSeed(
-//       seedColor: const Color(0xFF3AA1FF),
-//       brightness: Brightness.light,
-//     ).copyWith(
-//       surface: Colors.white,
-//       background: Colors.white,
-//       onSurface: Colors.black,
-//       onBackground: Colors.black,
-//     );
-
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Calendar',
-//       theme: ThemeData(
-//         useMaterial3: true,
-//         colorScheme: scheme,
-//         scaffoldBackgroundColor: Colors.white,
-//         dialogBackgroundColor: Colors.white,
-//         cardColor: Colors.white,
-//         canvasColor: Colors.white,
-//       ),
-//       home:  LoginScreen(),
-//     );
-//   }
-// }
-
