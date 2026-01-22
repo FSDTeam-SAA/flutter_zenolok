b0f47d25092a88d7eff049ae6216729c2a3c374e
Soykot Rahman Rodro <soykotrahman1212@gmail.com>
2026-01-22 15:15:05 +0600

    Update todo_category_repository.dart

--- lib/features/todos/domain/repositories/todo_category_repository.dart
+++ lib/features/todos/domain/repositories/todo_category_repository.dart
@@ -6,11 +6,20 @@ import '../../data/models/category_model.dart';
 abstract class TodoCategoryRepository {
   /// Fetches all todo categories
   /// Returns either a [NetworkFailure] or a list of [CategoryModel]
-  Future<Either<NetworkFailure, NetworkSuccess<List<CategoryModel>>>> getAllCategories();
+  Future<Either<NetworkFailure, NetworkSuccess<List<CategoryModel>>>>
+  getAllCategories();
 
   /// Creates a new todo category
   /// Returns either a [NetworkFailure] or the created [CategoryModel]
   Future<Either<NetworkFailure, NetworkSuccess<CategoryModel>>> createCategory({
+    required String name,
+    required String color,
+  });
+
+  /// Updates an existing todo category
+  /// Returns either a [NetworkFailure] or the updated [CategoryModel]
+  Future<Either<NetworkFailure, NetworkSuccess<CategoryModel>>> updateCategory({
+    required String categoryId,
     required String name,
     required String color,
   });
