c6e155729df39019c2f73bdfcbd40d88bfd3b2ce
Soykot Rahman Rodro <soykotrahman1212@gmail.com>
2026-01-31 10:18:15 +0600

    Update todo_details_dialog.dart

--- lib/features/todos/presentation/widgets/todo_details_dialog.dart
+++ lib/features/todos/presentation/widgets/todo_details_dialog.dart
@@ -4,6 +4,7 @@ import 'package:intl/intl.dart';
 
 import 'package:flutter_zenolok/core/common/constants/app_images.dart';
 import '../../../home/presentation/widgets/date_time_widget.dart';
+import 'custom_switch.dart';
 
 class TodoDetailsDialog extends StatefulWidget {
   final String todoTitle;
@@ -480,44 +481,7 @@ class _TodoDetailsDialogState extends State<TodoDetailsDialog> {
   }
 }
 
-class CustomSwitch extends StatelessWidget {
-  final bool value;
-  final ValueChanged<bool> onChanged;
 
-  const CustomSwitch({super.key, required this.value, required this.onChanged});
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => onChanged(!value),
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 200),
-        width: 64,
-        height: 28,
-        padding: const EdgeInsets.all(3),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(100),
-          color: value ? const Color(0xFF34C759) : Colors.grey.shade300,
-        ),
-        child: AnimatedAlign(
-          duration: const Duration(milliseconds: 200),
-          alignment: value ? Alignment.centerRight : Alignment.centerLeft,
-          child: Container(
-            width: 39, // thumb width
-            height: 24, // thumb height
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(100), // radius
-              boxShadow: [
-                BoxShadow(color: Colors.black.withOpacity(0.15), blurRadius: 4),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
 
 class _AlarmOption extends StatelessWidget {
   final String title;
