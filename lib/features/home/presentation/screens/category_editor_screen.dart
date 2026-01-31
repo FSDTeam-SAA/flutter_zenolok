838151d1c5d07ef97d3fcba84b0d14452a7db497
Soykot Rahman Rodro <soykotrahman1212@gmail.com>
2026-01-31 12:31:05 +0600

    Create category_editor_screen.dart

---
+++ lib/features/home/presentation/screens/category_editor_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategoryEditorScreen extends StatelessWidget {
+  const CategoryEditorScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Category Editor")),
+      body: const Center(child: Text("Category Editor Placeholder")),
+    );
+  }
+}
